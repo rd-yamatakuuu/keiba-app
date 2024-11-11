df934d79f1b52707e35d94a3a39f3e2e6feb3b08
rd-yamatakuuu <orgchem.techac@gmail.com>
2024-11-11 23:53:07 +0900

    KA-13 #3 - fix screen top

---
+++ src/main/java/com/keiba_app/app/controller/IndexViewController.java
@@ -0,0 +1,18 @@
+package com.keiba_app.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class IndexViewController {
+
+    @GetMapping
+    public String showIndexView(Model model) {
+        model.addAttribute("message", "Welcome! Success!!");
+        return "index";
+    }
+}
